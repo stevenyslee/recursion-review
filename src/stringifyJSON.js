5ad3e77027df498975a98d8a78bbcbf590ff6600
stevenyslee <stevenyslee@gmail.com>
2018-06-11 11:32:42 -0700

    Pass array and obj tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -9,4 +9,36 @@ var stringifyJSON = function(obj) {
     }else if( typeof obj === "string"){
         return '"' + obj + '"';
     }
+    if( obj instanceof Array ){
+        var s = "[";
+        for(var i = 0; i < obj.length; i++){
+            s += stringifyJSON(obj[i]);
+            if(i !== obj.length-1){
+                s += ",";
+            }
+        }
+        s += "]";
+        return s;
+    }
+    if( obj instanceof Object){
+        var s = "{";
+        let keys = Object.keys(obj);
+        for( let i = 0; i < keys.length; i++ ){
+            s += stringifyJSON(keys[i]) + ":" +stringifyJSON(obj[keys[i]]);
+            if(i !== keys.length-1){
+                s += ",";
+            }
+            
+        }
+        return s + "}";
+        // for(var i in obj){
+        //     s += stringifyJSON(i) + ":" + stringifyJSON(obj[i]);
+        //     console.log(Object.keys(obj));
+        //     if(i !== Object.keys(obj)[obj.length-1]){
+        //         s += ", ";
+        //     }
+        // }
+        // s += "}";
+        // return s;
+    }
 };
