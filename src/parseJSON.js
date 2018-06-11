ee71e8eea23f8d0132d5d849cac618d0683e3349
stevenyslee <stevenyslee@gmail.com>
2018-06-11 16:27:44 -0700

    Modify parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,40 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  for(var i = 0; i < json.length; i++ ){
+    if( json[i] === '[' ){
+      let array = [];
+      var newStr = json.slice(1,json.length-1);
+      
+      var arrayElem = newStr.split(",");
+      for(var i = 0; i < arrayElem.length;i++){
+        array.push(parseJSON(arrayElem[i]));
+      }
+      return array;
+    } if(json[i] === '"'){
+      return json.slice(1).slice(0,s.length-1);
+    } if(json[i] === 'n'){
+      return null;
+    } if(json[i] === 't'){
+      return true;
+    } if(json[i] === 'f'){
+      return false;
+    } if( /[0-9]/.test(json[i]) || json[i] === '-'){
+      return parseFloat(json);
+    } if(json[i] === '{'){
+      let obj = {};
+      var newStr = json.slice(1,json.length-1);
+      var arrayElem = newStr.split(",");
+      for(var i = 0; i < arrayElem.length;i++){
+        var keyValue = arrayElem[i].split(':');
+        let key = parseJSON(keyValue[0]);
+        let value = parseJSON(keyValue[1]);
+        obj[key] = value;
+      }
+      return obj;
+    }
+    
+    
+  }
 };
+
